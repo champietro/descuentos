00769307f24a8f6669e7c0443e7107453ba424d2
Juan Pedro Alcuaz <champietro@users.noreply.github.com>
2021-10-10 10:03:00 -0300

    FINAL

--- descuentos.ts
+++ descuentos.ts
@@ -3,8 +3,10 @@ let precio = document.getElementById("precio");
 const descuento: number = 10;
 
 btnEnv.addEventListener("click", () => {
-  let precios: number = Number(precio.value);
-  let total: number = precios / descuento;
-  let precioF: number = precios - precioF;
-  console.log("el dato ingresado es ", precioF);
+  let precio1: number = Number(precio.value);
+  let total: number = precio1 / descuento;
+  let precioF: number = precio1 - total;
+  console.log("el dato ingresado es ", precio1, ".");
+  console.log("el descuento es del ", descuento, "%.");
+  console.log("el Valor final a pagar es de: ", precioF);
 });
